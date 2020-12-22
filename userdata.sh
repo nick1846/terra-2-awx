74577886aeb2409c347dde634b3afb32e2634492
nick1846 <73354114+nick1846@users.noreply.github.com>
2020-12-22 11:51:16 -0600

    install ansible

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 sudo apt update -y
-sudo apt install git -y
 sudo apt-get update -y
+sudo apt install git -y
+sudo apt install ansible
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -10,6 +11,6 @@ sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo git clone https://github.com/nick1846/docker.git
-cd /docker/docker-compose
-sudo docker-compose up -d
+#sudo git clone https://github.com/nick1846/docker.git
+#cd /docker/docker-compose
+#sudo docker-compose up -d

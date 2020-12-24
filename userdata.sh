0f3a1bef4c3aeb59cbf59e6b039eb3a4356dcd6d
nick1846 <73354114+nick1846@users.noreply.github.com>
2020-12-23 22:21:01 -0600

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -5,7 +5,6 @@ sudo yum install git -y
 sudo amazon-linux-extras install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
-sudo yum install -y python3.7
 sudo yum install -y  python3-pip
 python3 -m pip install --user --upgrade pip # Do not run pip as sudo. Do this instead.
 python3 -m pip install ansible

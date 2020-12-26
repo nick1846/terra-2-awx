2bfe89c06176f93864295d2c9ea3ce9783acf58b
nick1846 <73354114+nick1846@users.noreply.github.com>
2020-12-26 16:57:39 -0600

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
-#sudo yum update -y
-#sudo amazon-linux-extras install epel -y
-#sudo yum install git -y
-#sudo amazon-linux-extras install docker
-#sudo service docker start
-#sudo usermod -a -G docker ec2-user
-#sudo yum install -y  python3-pip
-#python3 -m pip install --user --upgrade pip # Do not run pip as sudo. Do this instead.
-#python3 -m pip install ansible
-#python3 -m pip install docker-compose
+sudo yum update -y
+sudo amazon-linux-extras install epel -y
+sudo yum install git -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo yum install -y  python3-pip
+python3 -m pip install --user --upgrade pip # Do not run pip as sudo. Do this instead.
+python3 -m pip install ansible
+python3 -m pip install docker-compose
 
-#mkdir home/ec2-user/awx
-#cd home/ec2-user/awx/
-#git clone https://github.com/nick1846/linux-users-role.git
-#git clone https://github.com/nick1846/awx-project.git
-#cd awx-project/installer/
-#ansible-playbook -i inventory install.yml
+mkdir home/ec2-user/awx
+cd home/ec2-user/awx/
+git clone https://github.com/nick1846/linux-users-role.git
+git clone https://github.com/nick1846/awx-project.git
+cd awx-project/installer/
+ansible-playbook -i inventory install.yml
 
 
-#cd ../../linux-users-role/
-#ansible-playbook -i hosts.yaml main.yaml
+cd ../../linux-users-role/
+ansible-playbook -i hosts.yaml main.yaml
 

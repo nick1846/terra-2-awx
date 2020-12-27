b6e09191cfb0a12ee05e21c4e3824ce967c76a45
nick1846 <73354114+nick1846@users.noreply.github.com>
2020-12-26 22:33:55 -0600

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -10,14 +10,14 @@ python3 -m pip install --user --upgrade pip # Do not run pip as sudo. Do this in
 python3 -m pip install ansible
 python3 -m pip install docker-compose
 
-mkdir home/ec2-user/awx
-cd home/ec2-user/awx/
+
+cd home/ec2-user/
 git clone https://github.com/nick1846/linux-users-role.git
-git clone https://github.com/nick1846/awx-project.git
-cd awx-project/installer/
+git clone https://github.com/nick1846/awx.git
+cd awx/installer/
 ansible-playbook -i inventory install.yml
 
 
-cd ../../linux-users-role/
+cd home/ec2-user/linux-users-role/
 ansible-playbook -i hosts.yaml main.yaml
 

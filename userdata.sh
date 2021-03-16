37be7c422961de68f59a08f96ae0a676c2c020a5
nick1846 <73354114+nick1846@users.noreply.github.com>
2021-03-15 22:17:07 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -4,6 +4,7 @@ sudo amazon-linux-extras install epel -y
 sudo yum install git -y
 sudo amazon-linux-extras install docker
 sudo service docker start
+sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 sudo yum install -y  python3-pip
 python3 -m pip install --user --upgrade pip # Do not run pip as sudo. Do this instead.

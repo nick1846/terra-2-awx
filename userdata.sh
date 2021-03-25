2b7c54136a9f1274cfa48c322d1c10968585689b
nick1846 <73354114+nick1846@users.noreply.github.com>
2021-03-25 00:08:01 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -2,7 +2,7 @@
 sudo yum update -y
 sudo amazon-linux-extras install epel -y
 sudo yum install git -y
-sudo amazon-linux-extras install docker
+sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
